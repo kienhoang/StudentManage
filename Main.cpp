1870b2f14dfbb17e219f5a9af8b1aa184ccd7900
Hoàng Kiên <hoanglieuphuong@gmail.com>
2015-08-22 23:02:49 +0700

    Hoàn thiện hàm Main với tham số console

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include "Student.h"
 #include "StudentConsole.h"
-#include "StringProcess.h"
 
-int main() {	
+int main(int argc, char **argv) {
 	StudentConsole student;
-	student.start_console();
+	switch (argc){
+	case 1:
+		student.start_console();
+		break;
+	case 2:
+		student.OpenList_console(argv[1]);
+		student.start_console();
+		break;
+	default:
+		for (int i = 1; i < argc; i++) {
+			student.AddList_console(argv[i]);
+		}
+		student.start_console();
+		break;
+	}
 	return 0;
 }
