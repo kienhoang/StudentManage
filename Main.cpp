4670b23a79e6ac6dc036b2bdc2dd88ae7aab15bd
Hoàng Kiên <hoanglieuphuong@gmail.com>
2015-08-22 17:57:30 +0700

    Hàm Main simple

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include "Student.h"
 #include "StudentConsole.h"
+#include "StringProcess.h"
 
 int main() {	
 	StudentConsole student;
-	student.AddStudent("Hoang Van", "Kien", 20142397);
-	student.AddStudent("Nguyen Thi", "Nhai", 20143274);
-	student.AddStudent("Le Van", "Hoang", 20154236);
-	student.AddStudent_console();
-	student.SortList(1);
-	student.viewlist_console();
-	student.FindName("Hoang");
+	student.start_console();
 	return 0;
 }
