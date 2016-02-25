69ecf64048c31b853b6a3901bc41cd1bc3554016
Hoàng Kiên <hoanglieuphuong@gmail.com>
2016-02-25 23:51:49 +0700

    25.02.2016
    Make Builder

--- build-win-g++.bat
+++ build-win-g++.bat
@@ -1,7 +1,8 @@
+mkdir Release
 g++ -std=c++11 -c StringProcess.cpp -fpic
-g++ -std=c++11 -shared -o StringProcess.dll StringProcess.o
+g++ -std=c++11 -shared -o Release/StringProcess.dll StringProcess.o
 g++ -std=c++11 -c Student.cpp -fpic
-g++ -std=c++11 -shared -o Student.dll -L. -lStringProcess  Student.o
+g++ -std=c++11 -shared -o Release/Student.dll -L. -lStringProcess  Student.o
 g++ -std=c++11 -c StudentConsole.cpp -fpic
-g++ -std=c++11 -shared -o StudentConsole.dll -L. -lStudent -lStringProcess StudentConsole.o
-g++ -std=c++11 -o StudentManagement -L. Main.cpp -lStudentConsole
+g++ -std=c++11 -shared -o Release/StudentConsole.dll -L. -lStudent -lStringProcess StudentConsole.o
+g++ -std=c++11 -o Release/StudentManagement -L. Main.cpp -lStudentConsole
